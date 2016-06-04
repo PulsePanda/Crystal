a4c5714876b19b7679428b09a92051afde692c3f
Austin VanAlstyne <ajv857@gmail.com>
2016-06-04 18:48:04 -0500

    Added TTS class
    Created and implemented a TTS class, as well as a testing class for it.
    We can use the SpeechDriver differently, however for now it's simply
    TTS. I dont like the way it handles creating a new object for speaking,
    but that's something that can easily change.
    
    Voice needs to change.

--- src/Utilities/Speech/SpeechDriver.java
+++ src/Utilities/Speech/SpeechDriver.java
@@ -1,5 +1,22 @@
 package Utilities.Speech;
 
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
 public class SpeechDriver {
 
+	private static final String VOICENAME_kevin = "kevin";
+	private String text; // string to speech
+
+	public SpeechDriver(String text) {
+		this.text = text;
+	}
+
+	public void speak() {
+		Voice voice;
+		VoiceManager voiceManager = VoiceManager.getInstance();
+		voice = voiceManager.getVoice(VOICENAME_kevin);
+		voice.allocate();
+		voice.speak(text);
+	}
 }
---
+++ src/Utilities/Speech/SpeechTestDriver.java
@@ -0,0 +1,7 @@
+package Utilities.Speech;
+
+public class SpeechTestDriver {
+	public static void main(String[] args) {
+		new SpeechDriver("Welcome to Crystal Home Systems!").speak();
+	}
+}
