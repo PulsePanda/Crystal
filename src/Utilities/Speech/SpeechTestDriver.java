6189cb6c5f94bf027785a5bd5c78459bebd68716
Austin VanAlstyne <ajv857@gmail.com>
2016-06-07 11:17:29 -0500

    Built a basic speech recognition class
    The Recognition class is basic at best, however it is able to detect
    words if you articulate clearly, and we can start and stop it. Next i'm
    going to improve it's accuracy and refine it's dictionary

--- src/Utilities/Speech/SpeechTestDriver.java
+++ src/Utilities/Speech/SpeechTestDriver.java
@@ -3,6 +3,7 @@ package Utilities.Speech;
 public class SpeechTestDriver {
 	public static void main(String[] args) {
 		SpeechDriver sd = new SpeechDriver();
-		sd.Speak("Welcome to Crystal Home Systems!");
+		// sd.Speak("Welcome to Crystal Home Systems!");
+		// sd.Listen();
 	}
 }
