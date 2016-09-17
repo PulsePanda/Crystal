b8633f57af65edc7ff9178c6d1b061d37d69336e
Austin VanAlstyne <ajv857@gmail.com>
2016-09-17 13:02:25 -0500

    Cleaned up testing functions in APIHandler

--- src/Utilities/APIHandler.java
+++ src/Utilities/APIHandler.java
@@ -6,11 +6,9 @@
 package Utilities;
 
 import java.net.URL;
-import java.util.Calendar;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -70,38 +68,5 @@ public class APIHandler {
 
     public String getCurrentURL() {
         return url;
-    }
-
-    public static void main(String[] args) {
-        APIHandler api = new APIHandler("http://api.openweathermap.org/data/2.5/forecast?id=5275191&appid=70546178bd3fbec19e717d754e53b129");
-        StringBuilder forecast = new StringBuilder();
-
-        JSONArray array = api.getJSONArray("list");
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject obj = array.getJSONObject(i);
-
-            // Filter out every 3 hour value other than the mid day
-            if (!obj.getString("dt_txt").contains("12:00:00")) {
-                continue;
-            }
-
-            int apiDate = obj.getInt("dt"); // Date pulled from forecast
-
-            // Set up the calendar object for the given date
-            Calendar date = Calendar.getInstance();
-            java.util.Date time = new java.util.Date((long) apiDate * 1000);
-            date.setTime(time);
-
-            //String weather = obj.getJSONObject("weather").getString("main");
-            String weather = obj.getJSONArray("weather").getJSONObject(0).getString("description");
-            int temp = (int) kelvinToF(obj.getJSONObject("main").getDouble("temp"));
-            forecast.append("Date: " + date.get(Calendar.MONTH) + " " + date.get(Calendar.DAY_OF_MONTH) + "\nCloud Cover: " + weather + "\nTemperature: " + temp + "\n\n");
-        }
-
-        JOptionPane.showMessageDialog(null, forecast.toString());
-    }
-
-    private static double kelvinToF(double kelvin) {
-        return 1.8 * (kelvin - 273) + 32;
     }
 }
