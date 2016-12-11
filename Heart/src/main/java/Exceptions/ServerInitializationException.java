1c204bf398b3a1556800c7b0ca96b0739674c985
Austin VanAlstyne <ajv857@gmail.com>
2016-12-11 01:43:13 -0600

    playing and tweaking

---
+++ Heart/src/main/java/Exceptions/ServerInitializationException.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class ServerInitializationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ServerInitializationException(String message) {
+		super(message);
+	}
+}
