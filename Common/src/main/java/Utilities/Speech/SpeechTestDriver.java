8ee301202e224b45f75aa26de7b7a40cfee906b5
Austin VanAlstyne <ajv857@gmail.com>
2016-12-11 02:05:05 -0600

    Updated Common files

--- Common/src/main/java/Utilities/Speech/SpeechTestDriver.java
+++
@@ -1,9 +0,0 @@
-package Utilities.Speech;
-
-public class SpeechTestDriver {
-	public static void main(String[] args) {
-		SpeechDriver sd = new SpeechDriver();
-		 sd.Speak("Welcome to Crystal Home Systems!");
-		 sd.Listen();
-	}
-}
