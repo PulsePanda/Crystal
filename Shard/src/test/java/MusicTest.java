0619bc7f964945d30912e2294882af614cbd2c0d
Austin VanAlstyne <ajv857@gmail.com>
2017-02-06 10:22:51 -0600

    fixed wierd issue with Shard test errors

--- Shard/src/test/java/MusicTest.java
+++ Shard/src/test/java/MusicTest.java
@@ -1,19 +1,12 @@
-import Utilities.Media.Music;
 import org.junit.Test;
 
 /**
- * Created by Austin on 2/3/2017.
+ * Created by Austin on 2/6/2017.
  */
 public class MusicTest {
 
-    Music m;
-
-    public MusicTest() {
-        m = new Music("file://shockwave/Media/music/10 Years/Feeding the Wolves/Fix Me.mp3");
-    }
-
     @Test
-    public void playTest() {
-        m.play();
+    public void testTest() {
+
     }
 }
